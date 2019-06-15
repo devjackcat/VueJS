02c9cdfe33023c32a1b333021252ecffd0e22f14
永平 <yongping@anve.com>
2019-06-16 00:09:26 +0800

    test

---
+++ axios-demo/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <button @click="test">TTTTT</button>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'App',
+  methods:{
+    test(){
+      axios({
+        method: 'post',
+        url:'http://localhost:3000/login',
+      }).then(function (response) {
+        console.log("-----response----");
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.log("-----error----");
+        console.log(error);
+      });;
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- node-axios/app.js
+++ node-axios/app.js
@@ -1,18 +1,7 @@
 const express = require('express')
 const app = express()
 const axios = require('axios')
-
 app.use(express.static('public'));
-
-// 自定义跨域中间件
-var allowCors = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  res.header('Access-Control-Allow-Credentials','true');
-  next();
-};
-app.use(allowCors);//使用跨域中间件
 
 var httpTool = {
   baseurl:'http://218.90.150.158:12000/',
@@ -51,7 +40,7 @@ var httpTool = {
     var result = data.match(/<\w*Result>(.*)<\/\w*Result>/g)[0]
     result = result.replace(/<[^>]*?>(.*?)/gi,'$1'); //删除左部
     result = result.replace(/(.*?)<\/[^>]*?>/gi,'$1'); //删除右部
-    return JSON.parse(result)
+    return result
   },
 
   getParamString:function(params){
@@ -67,28 +56,16 @@ var httpTool = {
   }
 }
 
-app.post('/login',function(req,res){
-  httpTool.post('Service1','Login_Check',{
-    Name:'1502009',
-    Pwd:'cfzzfc',
-    CompanyNo:'sjk008',
-    os:'ios'
-  })
-  .then(data => {
-    console.log('-----'+data);
-    res.json(data)
-  })
-  .catch(err => {
-    console.log('-----'+err);
-    res.json(error)
-  })
+httpTool.post('Service1','Login_Check',{
+  Name:'1502009',
+  Pwd:'cfzzfc',
+  CompanyNo:'sjk008',
+  os:'ios'
+})
+.then(data => {
+  console.log('-----'+data);
+})
+.catch(err => {
+  console.log('-----'+err);
 })
 
-app.post('/api',function(req,res){
-  var obj = {
-    userid:1,
-    username:'zhangsan'
-  }
-  res.json(obj) 
-})
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
